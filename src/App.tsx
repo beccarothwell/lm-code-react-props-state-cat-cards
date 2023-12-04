8add904d836c48ca80f64617d645363e42f9e8b2
Becca Rothwell <becca.rothwell@gmail.com>
2023-12-04 19:23:02 +0000

    feat: Complete activity 5 - add CatCardProps interface to cat_card.tsx, pass props to CatCard and use in return

--- src/App.tsx
+++ src/App.tsx
@@ -82,7 +82,6 @@ function App(): JSX.Element {
       birthYear: 2021,
     },
   ]);
-  console.log("Our pretties 😻: ", cats);
   return (
     <>
       <Navbar />
@@ -91,7 +90,12 @@ function App(): JSX.Element {
       <main>
         <div className="cards__wrapper">
           {cats.map((cat) => (
-            <CatCard />
+            <CatCard
+              name={cat.name}
+              species={cat.species}
+              favFoods={cat.favFoods}
+              birthYear={cat.birthYear}
+            />
           ))}
         </div>
       </main>
